a0dc5ed307b42f6952b6ee17fdcd9709ff905e2f
Jose Lopez <jooooeyla@outlook.com>
2019-07-01 00:11:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,10 +12,24 @@ function distanceFromHqInFeet(blockNumber) {
   return distanceFromHqInBlocks(blockNumber) * 264;
 }
 
-function distanceTravelledInFeet(start, finish) {
-  if (start < finish) {
-    return (finish - start) * 264;
+function distanceTravelledInFeet(start, end) {
+  if (start < end) {
+    return (end - start) * 264;
   } else {
-    return (start - finish) * 264;
+    return (start - end) * 264;
+  }
+}
+
+function calculatesFarePrice(start, end) {
+  const distance = distanceTravelledInFeet(start, end); 
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * distance;
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far'
   }
 }
